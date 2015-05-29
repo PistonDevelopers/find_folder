6ef568c9552c4e3a7eba3e3e838b9d5b50257b8b
mitchmindtree <mitchell.nordine@gmail.com>
2015-05-29 22:51:31 +1000

    Initial commit

---
+++ examples/test.rs
@@ -0,0 +1,12 @@
+
+extern crate find_folder;
+
+use find_folder::Search;
+
+fn main() {
+    println!("{:?}", Search::Parents(3).for_folder("src"));
+    println!("{:?}", Search::Kids(3).for_folder("examples"));
+    println!("{:?}", Search::Both(3, 3).for_folder("target"));
+}
+
+
