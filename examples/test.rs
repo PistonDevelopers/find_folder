db7a233ba38c1ca27c539983ff08507fb0a9aa3d
mitchmindtree <mitchell.nordine@gmail.com>
2015-05-29 23:20:15 +1000

    Add variants for choosing which order to search parents and kids

--- examples/test.rs
+++ examples/test.rs
@@ -7,6 +7,8 @@ fn main() {
     println!("{:?}", Search::Parents(3).for_folder("src"));
     println!("{:?}", Search::Kids(3).for_folder("examples"));
     println!("{:?}", Search::Both(3, 3).for_folder("target"));
+    println!("{:?}", Search::ParentsThenKids(3, 3).for_folder("src"));
+    println!("{:?}", Search::KidsThenParents(3, 3).for_folder("examples"));
 }
 
 
